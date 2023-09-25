1195af2d1c32f70750729dd21d51ebaa7d230b3e
faisalakb <faisalakb786@gmail.com>
2023-09-25 17:58:25 +0500

    Updated sql code

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals values(1,'Agumon','3-FEB-2020',0,1,10.23);
+insert into animals values(2,'Gabumon','15-NOV-2018',2,1,8.0);
+insert into animals values(3,'Pikachu','7-JAN-2021',1,0,15.04);
+insert into animals values(4,'Devimon','12-MAY-2017',5,1,11.0);
